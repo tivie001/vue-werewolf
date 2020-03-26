d2a2f69938d360da436bae0a1f86b01ce4e8d262
tylerivie <tyler.ivie@upma.org>
2020-03-25 18:50:22 -0600

    Hooked up Firebase hosting & database to project.

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -5,6 +5,24 @@
         {{title}}
       </v-col>
     </v-row>
+    <v-flex sm12 lg12 mb-3 pl-0 pr-4>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Role</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(player, index) in players" :key="index"  style="height: 75px">
+            <td>{{ player.name }}</td>
+            <td>{{ player.role }}</td>
+          </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-flex>
   </v-container>
 </template>
 
@@ -13,8 +31,16 @@
     name: 'FirstPage',
 
     data: () => ({
-      title: 'This is the first page'
+      title: 'Players'
     }),
+    computed: {
+      players(){
+        return this.$store.state.players
+      }
+    },
+    created() {
+      this.$store.dispatch('retrievePlayers');
+    }
   }
 </script>
 
